22dc1dfd6db3afd473170d40141a1197d8e09de4
Cabin <scmagic77@gmail.com>
2023-03-02 17:33:31 +0900

    android unit id

--- ios/Classes/SwiftAdnFlutterOfferwallPlugin.swift
+++ ios/Classes/SwiftAdnFlutterOfferwallPlugin.swift
@@ -1,8 +1,6 @@
 import Flutter
 import UIKit
 import BuzzvilSDK
-import AppTrackingTransparency
-import AdSupport
 
 public class SwiftAdnFlutterOfferwallPlugin: NSObject, FlutterPlugin {
     public static func register(with registrar: FlutterPluginRegistrar) {
@@ -36,7 +34,6 @@ public class SwiftAdnFlutterOfferwallPlugin: NSObject, FlutterPlugin {
         }
         
         BuzzvilCore.initWithAppKey(appKey)
-        requestPermission()
     }
     
     private func setUserId(_ call: FlutterMethodCall, _ result: @escaping FlutterResult) {
@@ -77,34 +74,5 @@ public class SwiftAdnFlutterOfferwallPlugin: NSObject, FlutterPlugin {
         }
         
         result(BuzzOfferwall.showOfferWall(withTitle: title))
-    }
-    
-    private func requestPermission() {
-        if #available(iOS 14, *) {
-            ATTrackingManager.requestTrackingAuthorization { status in
-                switch status {
-                case .authorized:
-                    // Tracking authorization dialog was shown
-                    // and we are authorized
-                    print("Authorized")
-                    
-                    // Now that we are authorized we can get the IDFA
-                    print(ASIdentifierManager.shared().advertisingIdentifier)
-                case .denied:
-                    // Tracking authorization dialog was
-                    // shown and permission is denied
-                    print("Denied")
-                case .notDetermined:
-                    // Tracking authorization dialog has not been shown
-                    print("Not Determined")
-                case .restricted:
-                    print("Restricted")
-                @unknown default:
-                    print("Unknown")
-                }
-            }
-        } else {
-            // Fallback on earlier versions
-        }
     }
 }
